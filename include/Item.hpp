094b03185eebeb261bb66a47c7a0de71f9d645db
saba <sababahrami202@gmail.com>
2025-06-06 13:08:19 +0330

    Add Item class #6

---
+++ include/Item.hpp
@@ -0,0 +1,23 @@
+#ifndef ITEM_HPP
+#define ITEM_HPP
+#include <string>
+#include <stdexcept>
+enum class ItemColor { Red , Blue , Yellow};
+class Item{
+ private:
+    ItemColor color;
+    int power;
+    std::string location;
+
+ public:
+    Item(ItemColor , int , const std::string&);
+    ItemColor getColor() const;
+    int getPower() const;
+    const std::string& getLocation() const;
+    void setLocation(const std::string&) ;
+
+
+
+
+};
+#endif
