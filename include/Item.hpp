3488b6160253b1558b943159cb8ad267282acdab
mary <maryam.nokohan@gmail.com>
2025-06-13 04:28:36 +0330

    fixed Item class and used smart ptrs #6

--- include/Item.hpp
+++ include/Item.hpp
@@ -1,24 +1,28 @@
 #ifndef ITEM_HPP
 #define ITEM_HPP
+#include <memory>
 #include <string>
 #include <stdexcept>
+class Location;
 enum class ItemColor { Red , Blue , Yellow};
-class Item{
+class Item :public std :: enable_shared_from_this<Item>
+{
  private:
+    std::string name;
     ItemColor color;
     int power;
-    std::string location;
+    std :: shared_ptr<Location> location;
+    std :: shared_ptr<Location> CurrentLocation;
 
  public:
-    Item(ItemColor , int , const std::string&);
+    Item(ItemColor , int , const std::string& , const std::string&);
     ItemColor getColor() const;
     int getPower() const;
-    const std::string& getLocation() const;
-    void setLocation(const std::string&) ;
+    const std::shared_ptr<Location>& getLocation() const;
+
+    const std::string& getName() const;
+    void setLocation(const std :: shared_ptr<Location>&) ;
     bool operator==(Item item);
-
-
-
 
 };
 #endif
--- src/Item.cpp
+++ src/Item.cpp
@@ -1,15 +1,15 @@
 #include "../include/Item.hpp"
-
- Item::Item(ItemColor color, int power, const std::string& location) : 
- color(color) {
+#include "../include/Location.hpp"
+ Item::Item(ItemColor color, int power, const std::string& loc , const std::string& name) : 
+ color(color) , name(name) {
     if(power<=0 || power>10){
         throw std::invalid_argument("Power must be between 1 and 10");
     }
-    if(location.empty()){
+    if(loc.empty()){
         throw std::invalid_argument("Location can't be empty");
     }
     this->power = power;
-    this->location = location ;
+     location=nullptr;
  }
 
 ItemColor Item::getColor() const{
@@ -19,16 +19,16 @@ ItemColor Item::getColor() const{
 int Item::getPower() const{
     return power;
 }
-const std::string& Item::getLocation() const{
+const std::shared_ptr<Location>& Item::getLocation() const{
     return location;
 
     }
-void Item::setLocation(const std::string& newLoc){
-    if(newLoc.empty()){
-        throw std::invalid_argument("New location can't be empty");
-    }
-    location = newLoc ;
-
+const std::string& Item::getName() const{
+    return name;
+}
+void Item::setLocation(const std::shared_ptr<Location>& newLoc){
+    location = newLoc;
+    location->AddItem(shared_from_this());
 }
     bool Item :: operator==(Item item){
         if(item.getColor() == color && item.getLocation() == location  && power == item.getPower())
