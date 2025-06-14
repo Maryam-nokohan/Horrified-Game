6d1e63502b7d4708028194ef85531674e146d820
mary <maryam.nokohan@gmail.com>
2025-06-14 15:32:54 +0330

    Update Terminal class #11

--- include/Terminal.hpp
+++ include/Terminal.hpp
@@ -3,7 +3,6 @@
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/screen/screen.hpp>
 #include <ftxui/component/component.hpp>
-#include "../include/Game.hpp"
 #include <vector>
 #include <string>
 #include <memory>
@@ -15,6 +14,7 @@ class ShowInTerminal{
   int MenuGenerator(const std :: vector <std :: string> Options);
   void StylizeTextBoard(const std :: string text);
   void ShowTerrorLevel(int);
+  std :: string GetInput(const std :: string output);
 
 };
 #endif
