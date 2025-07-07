7d6a91ca195aeb2e00e85e245afd8f49f6ac20b2
maryam-nokohan <maryam.nokohan@gmail.com>
2025-07-07 21:16:07 +0330

    added courier class #29

---
+++ include/Courier.hpp
@@ -0,0 +1,14 @@
+#ifndef COURIER_HPP
+#define COURIER_HPP
+#include "../include/Hero.hpp"
+#include <memory>
+#include <string>
+class Game;
+class Courier : public Hero
+{
+public:
+    Courier(std::shared_ptr<Location>);
+    std::string specialAction(Game &);
+};
+
+#endif
